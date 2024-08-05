88221010b97a3916973dad288a70ce13035bf995
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-05 14:48:39 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import asyncio
+import heapq
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    heapq.heapify(delays)
+    sorted_delays = [heapq.heappop(delays) for _ in range(len(delays))]
+    return sorted_delays
