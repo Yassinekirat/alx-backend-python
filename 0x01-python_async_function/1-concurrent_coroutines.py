5448c70d2a8de0d995f4a57b5bc5de90b97c3620
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-05 14:56:26 +0100

    still fixing task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
-    """execute multiple coroutines at the same time with async"""
+    """
+    Defines concurrent execution function
+    """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
     heapq.heapify(delays)
