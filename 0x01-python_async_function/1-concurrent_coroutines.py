8cca571459b97712358b4e216f9c5a30ad571266
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-05 14:50:30 +0100

    fixing all shebang for all tasks

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""execute multiple coroutines at the same time with async"""
 import asyncio
 import heapq
 wait_random = __import__('0-basic_async_syntax').wait_random
