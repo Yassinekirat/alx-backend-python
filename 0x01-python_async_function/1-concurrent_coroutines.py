69e816a86ae9a9c4c8548a201d30f50e7617aea4
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-05 14:51:07 +0100

    adding docs

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,6 +6,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
+    """execute multiple coroutines at the same time with async"""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
     heapq.heapify(delays)
