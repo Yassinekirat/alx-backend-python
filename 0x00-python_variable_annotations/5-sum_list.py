1274e4e29278d9838da312d42f1b60e67ddc8f05
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-06 10:04:26 +0100

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+""" list of floats"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """sum of list of floats"""
+    return sum(input_list)
