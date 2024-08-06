6345d93af474fe61b2c3d8e54516263997eb5e3f
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-06 16:04:14 +0100

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Complex types - functions"""
+    def multiplier_func(x: float) -> float:
+        return x * multiplier
+    return multiplier_func
