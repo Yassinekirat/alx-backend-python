16652a66204d23c5d6c854edf25b71dce9c3707c
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-06 17:02:03 +0100

    task 1 Async Comprehensions

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Async Comprehensions"""
+import asyncio
+import random
+from typing import Generator, List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Async Comprehensions"""
+    return([i async for i in async_generator()])
