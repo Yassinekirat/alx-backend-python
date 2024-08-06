04d8d5331c3ec7db2d63a8a2792f316ef771c8c4
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-06 16:35:32 +0100

    task 11

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+from typing import Union, Any, Sequence, Mapping, TypeVar
+T = TypeVar('T')
+
+
+def safely_get_value(
+    dct: Mapping[Any, Any],
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
+    if key in dct:
+        return dct[key]
+    else:
+        return default
