43ee462c2dfa29eb5f7027462578e6a0be2f80b6
YASSINE EL KIRAT <yassine.elkirat@gmail.com>
2024-08-06 17:14:49 +0100

    last task

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""Run time for four parallel comprehensions"""
+import asyncio
+import random
+import time
+from typing import Generator, List
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Run time for four parallel comprehensions"""
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    return time.perf_counter() - start
